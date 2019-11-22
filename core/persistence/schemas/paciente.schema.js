1a851185e5e4e191ef008e5b41dc0745181f88d1
luismario53 <luismariosandoval44@gmail.com>
2019-11-21 22:47:50 -0700

    Format

--- core/persistence/schemas/paciente.schema.js
+++ core/persistence/schemas/paciente.schema.js
@@ -4,17 +4,17 @@ const ObjectId = mongoose.Types.ObjectId;
 const SexoEnum = require("../enums/Sexo.enum");
 
 const UsuarioSchema = new Schema({
-    nombreUsuario: {type:String, required:true, maxlength:30, minlength:5},
-    contraseña: {type:String, required: true, maxlength:100, minlength:8},
-    nombre: { type:String, required:true, maxlength:50, minlength:1 },
-    edad: { type:Number, required:true, maxlength:50, minlength:1 },
-    sexo: { type: String, required:true, enum:SexoEnum.getAll()},
-    fechaDeNacimiento: {type:Date, required: true},
-    
-    generosMusicales: {type:[String], required:false},
-    generosDePeliculas: {type:[String], required: false},
-    amigos: {type: [{type:ObjectId, ref: "Usuario"}], required: false}
+    nombreUsuario: { type: String, required: true, maxlength: 30, minlength: 5 },
+    contraseña: { type: String, required: true, maxlength: 100, minlength: 8 },
+    nombre: { type: String, required: true, maxlength: 50, minlength: 1 },
+    edad: { type: Number, required: true, maxlength: 50, minlength: 1 },
+    sexo: { type: String, required: true, enum: SexoEnum.getAll() },
+    fechaDeNacimiento: { type: Date, required: true },
+
+    generosMusicales: { type: [String], required: false },
+    generosDePeliculas: { type: [String], required: false },
+    amigos: { type: [{ type: ObjectId, ref: "Usuario" }], required: false }
 
 }, { timestamps: true });
 
-module.exports= mongoose.model("Usuario", UsuarioSchema, "Usuarios");
+module.exports = mongoose.model("Usuario", UsuarioSchema, "Usuarios");
