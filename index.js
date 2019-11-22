41e5f387819c9f5e28cde25ac587f4f93816991a
luismario53 <luismariosandoval44@gmail.com>
2019-11-22 14:34:14 -0700

    Render html

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const logger = require("./utils/logger");
+const fs = require("fs");
 const app = express();
+const path = require("path");
 require("./core/persistence/connection/connection");
+
 
 //Settings
 app.set("port", process.env.PORT || 3000);
@@ -13,6 +16,10 @@ app.use(express.json());
 
 //Routes
 app.use("/", require("./routes/router"));
+
+app.get('/salud', function (req, res) {
+    res.sendFile(path.join(__dirname + '/core/views/login/Login.view.html'));
+});
 
 //Start server
 app.listen(app.get("port"), () => {
