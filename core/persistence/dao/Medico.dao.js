40ecb800343af4bb8cda11ab1efa48fb43967e06
luismario53 <luismariosandoval44@gmail.com>
2019-12-05 00:15:27 -0700

    tal vez ahora si jale

--- core/persistence/dao/Medico.dao.js
+++ core/persistence/dao/Medico.dao.js
@@ -1,6 +1,6 @@
 const MedicoModel = require("../schemas/Medico.schema");
-const bcrypt = require("bcrypt");
-const rounds = 10;
+/* const bcrypt = require("bcrypt");
+const rounds = 10; */
 
 module.exports.getById = async function (id) {
     const medico = await MedicoModel.findById(id);
@@ -13,20 +13,20 @@ module.exports.get = async function () {
 }
 
 module.exports.save = async function (medico) {
-    bcrypt.hash(medico.contraseñaMedico, rounds, function (err, hash) {
+    /* bcrypt.hash(medico.contraseñaMedico, rounds, function (err, hash) {
         medico.contraseñaMedico = hash;
         const newMedico = MedicoModel(medico);
         const result = newMedico.save();
         return result;
-    });
+    }); */
 
 }
 
 module.exports.login = async function (nombreUsuario, contraseña) {
-    const medico = await MedicoModel.find({ nombreUsuarioMedico: nombreUsuario });
+    /* const medico = await MedicoModel.find({ nombreUsuarioMedico: nombreUsuario });
     const match = await bcrypt.compare(contraseña, medico[0].contraseñaMedico);
     if (match) {
         return medico;
     }
-    return "medico";
+    return "medico"; */
 }
