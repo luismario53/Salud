fa8de5328b59df98f61bb5cbd194a232aa15472f
luismario53 <luismariosandoval44@gmail.com>
2019-12-05 18:32:18 -0700

    token y administrar

--- middlewares/token.js
+++ middlewares/token.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const secretPhrase = "1ts0nw3bTok3N";
 
 exports.generateToken = function (user) {
-    return jwt.sign(user, secretPhrase, { expiresIn: "5m" });
+    return jwt.sign(user, secretPhrase, { expiresIn: "15m" });
 }
 
 exports.validateToken = async function (token) {
