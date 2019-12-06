7704ea43d711d888155de0ce2998d35060457e54
Laboratorio 1821 <lv1821@itson.edu.mx>
2019-12-05 20:29:51 -0700

    cambios xd

--- core/persistence/dao/paciente.dao.js
+++ core/persistence/dao/paciente.dao.js
@@ -1,6 +1,4 @@
 const PacienteModel = require("../schemas/Paciente.schema");
-const bcrypt = require("bcrypt");
-const rounds = 10;
 
 module.exports.getById = async function (id) {
     const usuario = await PacienteModel.findById(id);
@@ -13,21 +11,14 @@ module.exports.get = async function () {
 }
 
 module.exports.save = async function (paciente) {
-    bcrypt.hash(paciente.contraseña, rounds, function (err, hash) {
-        paciente.contraseña = hash;
-        const newPaciente = PacienteModel(paciente);
-        const result = newPaciente.save();
-        return result;
-    });
+    const newPaciente = PacienteModel(paciente);
+    const result = await newPaciente.save();
+    return result;
 }
 
 module.exports.login = async function (nombreUsuario, contraseña) {
-    const usuario = await PacienteModel.find({ nombreUsuario: nombreUsuario });
-    const match = await bcrypt.compare(contraseña, usuario[0].contraseña);
-    if (match) {
-        return usuario;
-    }
-    return "usuario";
+    const usuario = await PacienteModel.find({ nombreUsuario: nombreUsuario, contraseña: contraseña });
+    return usuario;
 }
 
 module.exports.subirDocumento = async function (idPaciente, documento) {
